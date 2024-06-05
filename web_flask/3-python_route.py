c12a841f82519fc5a1c81d707f827f509e8762e6
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-06-05 17:56:15 +0100

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module contains hello function for simple flask app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_fun(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_more_fun(text):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
